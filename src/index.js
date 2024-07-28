3279660bc7f85758863def72d11d91d4f15dd5eb
SuSuBea <suhajdabea>
2024-07-28 13:02:58 +0200

    feat: display cards with axios

--- src/index.js
+++ src/index.js
@@ -4,11 +4,14 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { AdatProvider } from './context/adatContext';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <AdatProvider>
+      <App />
+    </AdatProvider>
   </React.StrictMode>
 );
 
--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -3,14 +3,19 @@ import Menu from '../components/Menu';
 import Search from '../components/Search';
 import Sortby from '../components/Sortby';
 import Cards from '../components/Cards';
+import useAdatContext from "../context/adatContext";
 
 export default function Dashboard(){
+    const {characterList} = useAdatContext();
     return(
         <>
         <Menu />
         <Search />
         <Sortby />
-        <Cards />
+        <div className='cards'>
+        {characterList.map((element, index) => { return <Cards adat={element} key={index} />})}
+        </div>
+        <button>Load more</button>
         </>
     );
 }
